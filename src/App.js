d22587c28f245cc38a90c68bb997a2fefb53a6cb
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-07-03 20:03:01 +0530

    update app routing

--- src/App.js
+++ src/App.js
@@ -8,21 +8,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
-
 const App = () => {
 
-    return(
+  return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
 
-        <Route path="/weather/:data" element={<WeatherData />} />
+        <Route path="/:country/:temperature/:minTemp/:maxTemp/:pressure/:humidity/:visibility/:wind/:sunrise/:sunset/:weatherDescription/:weatherIcon/:city" element={<WeatherData />} />
       </Routes>
     </Router>
   );
 };
 
 export default App;
-
-
-
