8de18fd807264f754f37d08129c4d6a5da020044
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-07-07 14:14:31 +0530

    remove unwanted imports and optimize all imports
    - [x] remove duplicate API requests
    
    - [x] optimize application performance
    
    - [x] define return jsx template under displayWeatherData() function

--- src/components/SingleCard/singleCard.jsx
+++ src/components/SingleCard/singleCard.jsx
@@ -1,91 +1,100 @@
-import React from 'react';
-
-import { Time_Formatter } from '../../services/Functions';
-
-import { timeFormate } from '../../services/Functions';
-
-import { RandomColor } from '../../services/Functions';
-
-import { Link } from 'react-router-dom';
-
+import React, { useEffect, useState } from "react";
+import { fetchWeatherData } from "../../services/ApiHandler";
+import { Link } from "react-router-dom";
+import { RandomColor, timeFormat, Time_Formatter } from "../../services/Functions";
 import { useParams } from 'react-router-dom';
 
-function SingleCard(props) {
+function SingleCard(){
+    const { cityCode } = useParams();
+    const [weatherData, setWeatherData] = useState(null);
 
-    const { country, temperature, minTemp, maxTemp, pressure, humidity, visibility, wind, sunrise, sunset, weatherDescription, weatherIcon, city } = useParams();
+    useEffect(() => {
+        const getWeatherData = async () => {
+            try {
+                const weatherData = await fetchWeatherData(cityCode);
+                setWeatherData(weatherData?.list);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        cityCode && getWeatherData();
+    }, [cityCode]);
 
-    const time = timeFormate();
 
-    const randomColor = RandomColor();
+    function displayWeatherData(){
+        const [
+            {
+                name,
+                visibility,
+                main: { temp, temp_min, temp_max, pressure, humidity },
+                wind: { speed },
+                sys: { sunrise, sunset },
+                weather: [{ description, icon }],
+            },
+        ] = weatherData;
 
-    const sunrise_ = Time_Formatter(sunrise);
-
-    const sunset_ = Time_Formatter(sunset);
-
-    const myStyles = {
-        backgroundSize: 'cover',
-        backgroundColor: randomColor
-    };
-
-    return (
-        <div class="container">
-            <div class="row">
-                <div class="col-12 d-flex justify-content-center align-items-center">
-                    <div className="card weather-card" >
-                        <div className="card-head" style={myStyles}>
-                            <div class="mt-3 ml-4">
+        return (
+            <div className="container">
+                <div id="cards" className="d-flex justify-content-center">
+                    <div className="" id="weather-container">
+                        <div>
+                            <div
+                                className="card weather-card"
+                                style={{
+                                    background: `url(../../../../../images/1.png) no-repeat, ${RandomColor()}`,
+                                }}
+                            >
                                 <Link to={`/`} className="links">
-                                    <b>
-                                        <i class="bi bi-arrow-left back-icon mx-3 back-icon"></i>
-                                    </b>
+                                    <i className="bi bi-arrow-left back-icon px-2"></i>
                                 </Link>
-                            </div>
-                            <div className="container text-center">
-                                <div className="row mt-5">
-                                    <h1>{city},{country}</h1>
-                                    <h5 className="list-fonts">{time}</h5>
-                                    <div className="row mt-4 mb-4">
-                                        <div className="col-sm-6  border-end">
-                                            <img src={`http://openweathermap.org/img/w/${weatherIcon}.png`} alt="weather-icon" />
-                                            <h5>{weatherDescription}</h5>
-                                        </div>
-                                        <div className="col-sm-6">
-                                            <h1>{temperature} &#8451;</h1>
-                                            <h6 className="list-fonts"><b>Temp Min: </b>{minTemp} &#8451;</h6>
-                                            <h6 className="list-fonts"><b>Temp Max: </b>{maxTemp} &#8451;</h6>
+                                <div>
+                                    <div className="container text-center">
+                                        <div className="row mt-5">
+                                            <div className="col-sm-6">
+                                                <h3>{name}</h3>
+                                                <h5>{timeFormat()}</h5>
+                                                <img
+                                                    src={`http://openweathermap.org/img/w/${icon}.png`}
+                                                    alt="weather-icon"
+                                                />
+                                                <h6>{description}</h6>
+                                            </div>
+                                            <div className="col-sm-6">
+                                                <h2>{Math.round(temp)} &#8451;</h2>
+                                                <h6>{`Temp Min: ${Math.round(temp_min)}`} &#8451;</h6>
+                                                <h6>{`Temp Max: ${Math.round(temp_max)}`} &#8451;</h6>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="card-body rounded-bottom">
-                            <div className="container text-center">
-                                <div className="row">
-                                    <div className="col-sm-4 border-end">
-                                        <h6 className="list-font"><b>Pressure: </b>{pressure} Hpa</h6>
-                                        <h6 className="list-font"><b>Humidity: </b>{humidity}%</h6>
-                                        <h6 className="list-font"><b>Visibility: </b>{visibility}Km</h6>
-                                    </div>
-                                    <div className="col-sm-4 border-end">
-                                        <div className="mt-2">
-                                            <i className="bi bi-arrow-up-right-circle-fill icon"></i>
-                                            <h5 className="list-fonts"><b></b>{wind}</h5>
+                                <div className="card-body">
+                                    <div className="container text-center">
+                                        <div className="row">
+                                            <div className="col-sm-4 border-end">
+                                                <h6>{`Pressure: ${pressure} Hpa`}</h6>
+                                                <h6>{`Humidity: ${humidity}%`}</h6>
+                                                <h6>{`Visibility: ${visibility}Km`}</h6>
+                                            </div>
+                                            <div className="col-sm-4 border-end">
+                                                <i className="bi bi-arrow-up-right-circle-fill"></i>
+                                                <h5>{`Wind: ${speed} m/s`}</h5>
+                                            </div>
+                                            <div className="col-sm-4">
+                                                <h6>{`Sunrise: ${Time_Formatter(sunrise)}`}</h6>
+                                                <h6>{`Sunset: ${Time_Formatter(sunset)}`}</h6>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="col-sm-4 mt-4">
-                                        <h6 className="list-fonts"><b>Sunrise: </b>{sunrise_}</h6>
-                                        <h6 className="list-fonts"><b>Sunset: </b>{sunset_}</h6>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
-        </div>
-    );
+        );
+    };
+
+    return weatherData && cityCode ? (displayWeatherData()) : (<div>Loading Please Wait...</div>);
 }
 
 export default SingleCard;
-
