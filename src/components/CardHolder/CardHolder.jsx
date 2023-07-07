34ac26eec785e5fca9d80ff66e8aa1b8d38e0b0e
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-07-07 18:44:50 +0530

    local storage operations are encapsulated in the localStorageUtils.js

--- src/components/CardHolder/CardHolder.jsx
+++ src/components/CardHolder/CardHolder.jsx
@@ -1,57 +1,45 @@
-import React, { useState, useEffect } from "react";
+import React, {useEffect, useState} from "react";
 import cities from "./cities.json";
 import Card from "../Card/card";
-import { fetchWeatherData } from "../../services/ApiHandler";
+import {fetchWeatherData} from "../../services/ApiHandler";
+import {getLocalStorageData, setLocalStorageData} from "../../utils/LocalStorageUtils";
 
 const CardHolder = () => {
-  const [weatherData, setWeatherData] = useState([]);
+    const [weatherData, setWeatherData] = useState([]);
 
-  useEffect(() => {
-    const getWeatherData = async () => {
-      const cityCodes = cities.List.map((city) => city.CityCode);
-      try {
-        // Check if data is available in local storage
-        const cachedData = localStorage.getItem("weatherData");
-        if (cachedData) {
-          const parsedData = JSON.parse(cachedData);
-          const currentTime = new Date().getTime();
-          const { timestamp, data } = parsedData;
-          // Check if cached data is not expired (5 minutes expiration)
-          if (currentTime - timestamp < 5 * 60 * 1000) {
-            setWeatherData(data);
-            return;
-          }
-        }
-        // Fetch data from API
-        const data = await fetchWeatherData(cityCodes);
-        // store data in local storage
-        const cachedDataToStore = {
-          timestamp: new Date().getTime(),
-          data: data?.list,
+    useEffect(() => {
+        const getWeatherData = async () => {
+            const cityCodes = cities.List.map((city) => city.CityCode);
+            try {
+                const cachedData = getLocalStorageData();
+                if (cachedData) {
+                    setWeatherData(cachedData);
+                    return;
+                }
+                const data = await fetchWeatherData(cityCodes);
+                setLocalStorageData(data);
+                setWeatherData(data?.list);
+            } catch (error) {
+                console.log("Error Occurred: " + error);
+            }
         };
-        localStorage.setItem("weatherData", JSON.stringify(cachedDataToStore));
 
-        setWeatherData(data?.list);
-      } catch (error) {
-        console.log("Error Occurred: " + error);
-      }
-    };
-    cities?.List?.length !== 0 && getWeatherData();
-  }, []);
+        cities?.List?.length !== 0 && getWeatherData();
+    }, []);
 
-  const cardList = weatherData.map((cityData) => (
-    <Card key={cityData?.id} {...cityData} />
-  ));
+    const cardList = weatherData.map((cityData) => (
+        <Card key={cityData?.id} {...cityData} />
+    ));
 
-  const displayCards = () => (
-    <div id="cards" className="container mt-5 my-3">
-      <div className="row row-cols-1 row-cols-md-2 g-4" id="weather-container">
-        {cardList}
-      </div>
-    </div>
-  );
+    const displayCards = () => (
+        <div id="cards" className="container mt-5 my-3">
+            <div className="row row-cols-1 row-cols-md-2 g-4" id="weather-container">
+                {cardList}
+            </div>
+        </div>
+    );
 
-  return weatherData ? displayCards() : <div>Loading Data...</div>;
+    return weatherData ? displayCards() : <div>Loading Data...</div>;
 };
 
 export default CardHolder;
