c760bf8fcca605ec9060c07c1ecbadacc99191c8
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-07-07 15:14:14 +0530

    fix component import error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Routes from './Routes';
 import './App.css';
-import Header from '../components/Header/header';
-import Footer from '../components/Footer/footer';
+import Header from './components/Header/header';
+import Footer from './components/Footer/footer';
 
 const App = () => {
     return (
