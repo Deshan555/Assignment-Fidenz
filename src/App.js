d0aa98a1f44c42586af43db2b2afd27620d2ba9f
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-07-07 15:08:54 +0530

    for optimize application performance call Header and footer once in app.js file
    - reformat file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Routes from './Routes';
 import './App.css';
+import Header from '../components/Header/header';
+import Footer from '../components/Footer/footer';
 
 const App = () => {
     return (
-    <div className="App">
-      <Routes />
-    </div>
-  );
+        <div className="App">
+            <Header/>
+            <Routes/>
+            <Footer/>
+        </div>
+    );
 };
 
 export default App;
