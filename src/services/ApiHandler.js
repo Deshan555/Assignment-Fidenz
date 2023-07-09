38a78099ff0feec952187bb9f7f2f9982555edcf
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-07-09 21:09:24 +0530

    remove unwanted comments and code lines

--- src/services/ApiHandler.js
+++ src/services/ApiHandler.js
@@ -1,27 +1,5 @@
 import {getWeatherDataByIds} from '../utils/APIHelper';
 
-/*export function fetchWeatherData(city) {
-
-    const weatherUrl = getWeatherUrl(city);
-
-    return fetch(weatherUrl)
-        .then(response => response.json())
-        .catch(error => {
-            console.error("Error fetching weather data:", error);
-        });
-}
-
-
-
-export function fetchWeatherData(ids) {
-    try {
-        const response = await fetch(`${getWeatherDataByIds}&id=${ids.toString()}`);
-        return await response.json();
-    } catch (error) {
-        console.error("Error fetching weather data:", error);
-    }
-}*/
-
 export const fetchWeatherData = async (ids) => {
     try {
         const response = await fetch(`${getWeatherDataByIds}&id=${ids.toString()}`);
