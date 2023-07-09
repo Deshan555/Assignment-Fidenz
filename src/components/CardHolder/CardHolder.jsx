448f4a21629977221aac71abf89b85ac11dfd4a9
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-07-09 21:16:33 +0530

    improve file readability and enhance performance of the code

--- src/components/CardHolder/CardHolder.jsx
+++ src/components/CardHolder/CardHolder.jsx
@@ -1,45 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import cities from "./cities.json";
 import Card from "../Card/card";
-import { fetchWeatherData } from "../../services/ApiHandler";
-import { getLocalStorageData, setLocalStorageData, } from "../../utils/LocalStorageUtils";
+import {fetchWeatherData} from "../../services/ApiHandler";
+import {getLocalStorageData, setLocalStorageData,} from "../../utils/LocalStorageUtils";
 
 const CardHolder = () => {
-  const [weatherData, setWeatherData] = useState([]);
+    const [weatherData, setWeatherData] = useState([]);
 
-  useEffect(() => {
-    const getWeatherData = async () => {
-      const cityCodes = cities.List.map((city) => city.CityCode);
-      try {
+    useEffect(() => {
+
         const cachedData = getLocalStorageData();
         if (cachedData) {
-          setWeatherData(cachedData);
-          return;
+            setWeatherData(cachedData);
+            return;
         }
-        const data = await fetchWeatherData(cityCodes);
-        setLocalStorageData(data);
-        setWeatherData(data?.list);
-      } catch (error) {
-        console.log("Error Occurred: " + error);
-      }
+
+        (async () => {
+            const data = await getWeatherData();
+            setLocalStorageData(data);
+            setWeatherData(data?.list);
+        })();
+
+    }, []);
+
+    const getWeatherData = async () => {
+        const cityCodes = cities.List.map((city) => city.CityCode);
+        try {
+            return await fetchWeatherData(cityCodes);
+        } catch (error) {
+            console.log("Error Occurred: " + error);
+        }
     };
 
-    cities?.List?.length !== 0 && getWeatherData();
-  }, []);
+    const cardList = weatherData.map((cityData) => (
+        <Card key={cityData?.id} {...cityData} />
+    ));
 
-  const cardList = weatherData.map((cityData) => (
-    <Card key={cityData?.id} {...cityData} />
-  ));
+    const displayCards = () => (
+        <div id="cards" className="container mt-5 my-3">
+            <div className="row row-cols-1 row-cols-md-2 g-4" id="weather-container">
+                {cardList}
+            </div>
+        </div>
+    );
 
-  const displayCards = () => (
-    <div id="cards" className="container mt-5 my-3">
-      <div className="row row-cols-1 row-cols-md-2 g-4" id="weather-container">
-        {cardList}
-      </div>
-    </div>
-  );
-
-  return weatherData ? displayCards() : <div>Loading Data...</div>;
+    return weatherData ? displayCards() : <div>Loading Data...</div>;
 };
 
 export default CardHolder;
