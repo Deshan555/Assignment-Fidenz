7603d8da69a5fe4d2d54f6972672d8b20f21a288
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-07-16 19:30:10 +0530

    reformat footer.jsx file

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Footer() {
     return (
         <footer className="footer mt-5 py-3 bg-dark">
-            <div className="container">
-                <span className="text-light"><center>Fidenz Academy - 2023</center></span>
+            <div className="container d-flex justify-content-center">
+                <span className="text-light">Fidenz Academy - 2023</span>
             </div>
         </footer>
     );
